59db57baddd47c7b91e6cb3f364b3693fc892ee2
Hooman Shykhipour <59225708+HoManCode@users.noreply.github.com>
2024-01-18 10:35:04 +1100

    creating article list and page not found page

--- front-end/bloggy/src/pages/ArticlePage.js
+++ front-end/bloggy/src/pages/ArticlePage.js
@@ -1,6 +1,20 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+import NotFoundPage from "./NotFoundPage";
 const ArticlePage = () =>{
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId);
+    
+    if(!article){
+        return <NotFoundPage />
+    }
     return(
-        <h1>This is the article page!!!</h1>
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map(paragraph => (
+                <p key={paragraph}>{paragraph}</p>
+            ))}
+        </>
     )
 }
 
