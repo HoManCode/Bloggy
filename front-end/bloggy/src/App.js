0a850624d1221d4e2c057869a95e90e2933d6cbc
Hooman Shykhipour <59225708+HoManCode@users.noreply.github.com>
2024-01-17 15:21:09 +1100

    create pages and NavBar

--- front-end/bloggy/src/App.js
+++ front-end/bloggy/src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
+import NavBar from "./NavBar";
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticleListPage from "./pages/ArticleListPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Awsome Blog</h1>
-      <div id="page-body">Welcome to my blog!!!</div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticleListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
