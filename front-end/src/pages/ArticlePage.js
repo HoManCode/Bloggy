9d47582e138e5edeccce98955285e855b53c3dc3
Hooman Shykhipour <59225708+HoManCode@users.noreply.github.com>
2024-01-23 16:20:32 +1100

    axios, upvotes and comments have been added to the front-end

---
+++ front-end/src/pages/ArticlePage.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+import NotFoundPage from "./NotFoundPage";
+import axios from 'axios';
+import CommentsList from "../components/CommentsList";
+
+const ArticlePage = () =>{
+    const [articleInfo, setArticleInfo ] = useState ({ upvotes: 0, comments:[] });
+    const { articleId } = useParams();
+    useEffect(() => {
+        const loadArticleInfo = async () =>{
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+    },[])
+    
+    const article = articles.find(article => article.name === articleId);
+    
+    if(!article){
+        return <NotFoundPage />
+    }
+    return(
+        <>
+            <h1>{article.title}</h1>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+            {article.content.map(paragraph => (
+                <p key={paragraph}>{paragraph}</p>
+            ))}
+            <CommentsList comments={articleInfo.comments} />
+        </>
+    )
+}
+
+export default ArticlePage;
