8d276f29f9df2fc2095543dff5f111d6b8148db4
Hooman Shykhipour <59225708+HoManCode@users.noreply.github.com>
2024-01-25 17:25:57 +1100

    create useUser hook and hide upvote and comment id the user is not logged in

--- front-end/src/pages/ArticlePage.js
+++ front-end/src/pages/ArticlePage.js
@@ -5,10 +5,13 @@ import NotFoundPage from "./NotFoundPage";
 import axios from 'axios';
 import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentFrom";
+import useUser from "../hooks/useUser";
 
 const ArticlePage = () =>{
     const [articleInfo, setArticleInfo ] = useState ({ upvotes: 0, comments:[] });
     const { articleId } = useParams();
+    const { user, isLoading } = useUser();
+
     useEffect(() => {
         const loadArticleInfo = async () =>{
             const response = await axios.get(`/api/articles/${articleId}`);
@@ -33,16 +36,20 @@ const ArticlePage = () =>{
         <>
             <h1>{article.title}</h1>
             <div className="upvote-section">
-                <button onClick={addUpvote}>Upvote</button>
+                { user 
+                ? <button onClick={addUpvote}>Upvote</button>
+                : <button>Log in to upvote</button>}
                 <p>This article has {articleInfo.upvotes} upvote(s)</p>
             </div>
             
             {article.content.map(paragraph => (
                 <p key={paragraph}>{paragraph}</p>
             ))}
-            <AddCommentForm 
-                articleName={articleId}
-                onArticleUpdate={updatedArticle => setArticleInfo(updatedArticle)}/>
+            {user
+                ? <AddCommentForm 
+                    articleName={articleId}
+                    onArticleUpdate={updatedArticle => setArticleInfo(updatedArticle)}/>
+                : <button>Log in to write a comment</button>}
             <CommentsList comments={articleInfo.comments} />
         </>
     )
